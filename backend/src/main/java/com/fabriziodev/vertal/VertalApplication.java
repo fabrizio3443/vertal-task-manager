31cc8ec503dc053258592a1d3502974793d6eae4
Fabrizio.dev <fabriziopalenque29@protonmail.com>
2026-02-14 15:41:39 -0400

    chore: restructured repo into monorepo

---
+++ backend/src/main/java/com/fabriziodev/vertal/VertalApplication.java
@@ -0,0 +1,13 @@
+package com.fabriziodev.vertal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VertalApplication.class, args);
+	}
+
+}
