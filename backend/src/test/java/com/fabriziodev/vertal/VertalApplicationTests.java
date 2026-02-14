bbb4b77d12c867d242e3f044a680bcc3aa75a6ae
Fabrizio.dev <99894561+fabrizio3443@users.noreply.github.com>
2026-02-14 15:41:39 -0400

    chore: restructured repo into monorepo

---
+++ backend/src/test/java/com/fabriziodev/vertal/VertalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabriziodev.vertal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VertalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
